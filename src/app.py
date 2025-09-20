aa870fabf4cc49da5e6c24b4feba8ffe72a38bfd
achyu-dev <achyugeorge7744@gmail.com>
2025-09-20 14:51:49 +0530

    render prod 2

--- src/app.py
+++ src/app.py
@@ -199,4 +199,5 @@ def upload_status():
     return jsonify(uploaded_files)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
